7bb98e0ed5ead533ed0d3bf31b3ac0ed4d048510
exetn. <mariadosocorrogomes1808@gmail.com>
2025-05-15 23:18:54 -0300

    Add files via upload

---
+++ notify/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version "bodacious"
+game "gta5"
+lua54 "yes"
+
+ui_page "web-side/ui.html"
+
+client_scripts {
+    "client-side/*"
+}
+
+server_scripts {
+    "server-side/*"
+}
+
+files {
+    "web-side/**/*"
+}
